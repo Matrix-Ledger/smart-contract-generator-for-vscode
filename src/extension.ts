4c7d8081da91c87bf890393a23a7755fd187beed
Mihai-Tudor Popescu <mihai.tudor.popescu@gmail.com>
2024-04-13 13:27:27 +0300

    Skeleton

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import axios from 'axios';
+import * as config from './config.json';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,17 +12,30 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "smart-contract-generator" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('smart-contract-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from smart-contract-generator!');
+	let disposable = vscode.commands.registerCommand('smart-contract-generator.generateSmartContract', async () => {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			vscode.window.showInformationMessage('Open a file to generate code');
+			return;
+		}
+
+		const prompt = editor.document.getText(editor.selection);
+
+		try {
+			const response = await axios.post(config.backendUrl, { prompt }, {
+				headers: { 'Content-Type': 'application/json' }
+			});
+			editor.edit(editBuilder => {
+				editBuilder.replace(editor.selection, response.data.code);
+			});
+		} catch (error) {
+			console.error("Error with custom backend:", error);
+			vscode.window.showErrorMessage("Failed to generate code. See console for details.");
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
